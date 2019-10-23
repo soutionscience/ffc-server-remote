0683887684fe09d0d6d7d82f63805298d8db2bc6
soutionscience <marshnjagi@yahoo.com>
2019-10-23 12:51:37 +0300

    added cors stuff

--- app.js
+++ app.js
@@ -41,6 +41,15 @@ let getLocal = require('./scripts/getLocalValue')
 var app = express();
 app.use(cors())
 
+// cross origin stuff
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 
 //passport stuff
 app.use(passport.initialize());
